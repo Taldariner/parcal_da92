7924cab266db789af7e9506fc6a6e4a8a21f2085
Taldariner <71024372+Taldariner@users.noreply.github.com>
2022-06-16 14:56:07 +0300

    Add files via upload

---
+++ Client_1/Program.cs
@@ -0,0 +1,6 @@
+﻿using TCP_client;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+var client = new Client("Student 1");
+client.RunCLient();
---
+++ Client_2/Program.cs
@@ -0,0 +1,6 @@
+﻿using TCP_client;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+var client = new Client("Student 2");
+client.RunCLient();
